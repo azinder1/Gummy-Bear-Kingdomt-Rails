ad8dd7d10674c3cd29c9e200bc864455a3a091ac
Adam Zinder <adam.zinder@gmail.com>
2017-02-10 08:12:35 -0800

    first commit with file structure

---
+++ db/migrate/20170210160440_product.rb
@@ -0,0 +1,10 @@
+class Product < ActiveRecord::Migration[5.0]
+  def change
+    create_table Products do |t|
+      t.column :name, :string
+      t.column :cost, :string
+      t.column :country_origin, :string
+      t.column :description, :string
+    end
+  end
+end
